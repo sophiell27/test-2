9a2c9834b30fd6732aa52754c1a9cbe3672f8b39
Sophie Lam <sophie.lam0207@gmail.com>
2025-03-05 18:59:07 +0800

    feat: add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import './App.css';
 import Home from './pages/Home';
 import About from './pages/About';
-import { Route, Routes } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <Router>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
       </Routes>
-    </>
+    </Router>
   );
 }
 
