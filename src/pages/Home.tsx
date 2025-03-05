e2b912430645b03c7ce1c5ff90b9591d3a709dfa
Sophie Lam <sophie.lam0207@gmail.com>
2025-03-05 19:05:59 +0800

    feat: add path to about

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,15 @@
+import { useNavigate } from 'react-router-dom';
+
 const Home = () => {
+  const navigate = useNavigate();
+  const onClick = () => {
+    navigate('/about');
+  };
   return (
     <div>
       <h1>Home</h1>
+      <div className=''></div>
+      <p onClick={onClick}>go to about </p>
     </div>
   );
 };
